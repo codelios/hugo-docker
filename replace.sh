9ca0a3a1b6375d05243d892ebe00aba378b2b70d
Karthik K <karthiknetworklink+code@gmail.com>
2019-10-04 00:13:40 +0530

    fix: script

--- replace.sh
+++ replace.sh
@@ -3,8 +3,8 @@ if [ $# -ne 1 ]; then
     echo "Syntax: $0 <branch name>"
     exit 1
 fi
-sed -i 's/HUGO_VERSION=0.58.3/HUGO_VERSION=$1' alpine/Dockerfile
-sed -i 's/HUGO_VERSION=0.58.3/HUGO_VERSION=$1' debian/Dockerfile
+sed -i 's/HUGO_VERSION=0.58.3/HUGO_VERSION=$1/' alpine/Dockerfile
+sed -i 's/HUGO_VERSION=0.58.3/HUGO_VERSION=$1/' debian/Dockerfile
 git com -m "feat: set hugo version $1" .
 git push
 
